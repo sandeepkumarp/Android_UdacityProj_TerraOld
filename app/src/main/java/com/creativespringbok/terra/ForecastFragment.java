c1d17fdd8dbeee0db1aa063b0828b04bc298f777
Sandeep <sandeep.pilakhnawal@gmail.com>
2015-11-03 11:39:01 +0530

    Added detail activity for weather details of a particular day. + Some minor tweaks.

--- app/src/main/java/com/creativespringbok/terra/ForecastFragment.java
+++ app/src/main/java/com/creativespringbok/terra/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.creativespringbok.terra;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -91,6 +93,16 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String forecast = mForecastAdapter.getItem(i);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+
+            }
+        });
 
 
         return rootView;
--- app/src/main/java/com/creativespringbok/terra/MainActivity.java
+++ app/src/main/java/com/creativespringbok/terra/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Terra: The only weather app you need.", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Terra: The only weather app you need.", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
